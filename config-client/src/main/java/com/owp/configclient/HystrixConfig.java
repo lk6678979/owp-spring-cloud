e0d615a41f51c46652012f0a7c0c81900ec75878
DESKTOP-UQMP51V\liukai <liuk@televehicle.com>
2019-05-20 15:36:29 +0800

    no message

--- config-client/src/main/java/com/owp/configclient/HystrixConfig.java
+++ config-client/src/main/java/com/owp/configclient/HystrixConfig.java
@@ -1,26 +1,26 @@
-package com.owp.configclient;
-
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class HystrixConfig {
-    @Bean
-    public ServletRegistrationBean getServlet(){
-
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
-
-        registrationBean.setLoadOnStartup(1);
-
-        registrationBean.addUrlMappings("/actuator/hystrix.stream");
-
-        registrationBean.setName("HystrixMetricsStreamServlet");
-
-
-        return registrationBean;
-    }
-}
+//package com.owp.configclient;
+//
+//import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+//import org.springframework.boot.web.servlet.ServletRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configurationa
+//public class HystrixConfig {
+//    @Bean
+//    public ServletRegistrationBean getServlet(){
+//
+//        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+//
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+//
+//        registrationBean.setLoadOnStartup(1);
+//
+//        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+//
+//        registrationBean.setName("HystrixMetricsStreamServlet");
+//
+//
+//        return registrationBean;
+//    }
+//}
